940d04630a35d728e5270f8153457d8f1195e742
Abdelfatah Laktaoui <abdalfattahlaktaoui98@gmail.com>
2026-09-05 13:12:18 +0100

    start tokenizer training flow

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -1,9 +1,56 @@
+from typing import Dict, Tuple, Optional
+from collections import defaultdict
+from itertools import pairwise
+
+from .pre_tokenizer import PreTokenizer
+from .vocabulary import Vocabulary
+
+
 class Tokenizer:
-    def __init__(self):
-        pass
+    def __init__(self, vocabulary_size: int):
+        self.pre_tokenize = PreTokenizer()
+        self.vocab = Vocabulary()
+
+        self.vocabulary_size = vocabulary_size - 256
+        self.merges = dict()
+
+    def step(self):
+        winner = self._get_winner_pair()
+        self.merges.add(winner)
+        new_token = vocabulary.add_token(best_key)
+        slef._rewriting(new_token)
+
+    def _get_winner_pair(self) -> Optional[Tuple[int, int]]:
+        pairs = defaultdict(int)
+
+        # 1. Accumulate pair frequencies
+        for t, count in tokens.items():
+            # pairwise(t) generates consecutive (t[i], t[i+1]) in C
+            for pair in pairwise(t):
+                pairs[pair] += count
+
+        if not pairs:
+            return None
+
+        # 2. Extract max value and tie-break in pure C
+        max_val = max(pairs.values())
+        return min(k for k, v in pairs.items() if v == max_val)
+
+    def _rewriting(self, new_token: int):
+        for token in self.tokens:
+            i = 0
+            token = list(token)
+            while i < len(token):
+                if (token[i], token[i+1]) == best_pair:
+                    token[i:i+1] = new_token
+                i += 1
+
 
     def train(self, text_data, target_vocab_size):
-        pass
+        self.tokens = ptoken.pre_tokenize(text)
+        i = 0
+        while i < self.vocabulary_size:
+            self.step()
 
     def encode(self, text):
         pass
