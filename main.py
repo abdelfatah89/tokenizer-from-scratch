f98e7389efe0c58c70b84cc77024bf6047bb276d
Abdelfatah Laktaoui <abdalfattahlaktaoui98@gmail.com>
2026-09-05 13:12:02 +0100

    update main token pair demo

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+import numpy as np
+
 from src.pre_tokenizer import PreTokenizer
-from src.bytes_mapper import BytesMapper
+from src.vocabulary import Vocabulary
 
 
 def main():
     ptoken = PreTokenizer()
-    maper = BytesMapper()
+    vocabulary = Vocabulary()
+
+    vocab_id_to_byte = vocabulary.id_to_bytes
+    vocab_byte_to_id = vocabulary.bytes_to_id
+
     text = """The quick brown fox jumps over the lazy dog. The dog wasn't really that lazy, but the fox was very, very quick!
 In 2024, the fox said, "I'll do it again." And indeed, the fox jumped 10 times, then 20 times, then 30 times.
 
@@ -16,8 +22,23 @@ Why did the fox jump?
 The end. It's truly the end! Or is it? We'll see... we'll definitely see."""
 
     tokens = ptoken.pre_tokenize(text)
+    pairs = dict()
     for t in tokens:
-        print(maper.decode(list(t)), ":", tokens[t])
+        token_len = len(t)
+        if token_len == 1:
+            continue
+        for i in range(token_len):
+            if i + 1 < token_len:
+                pair = (t[i], t[i+1])
+                pairs[pair] = tokens[t]
+
+    if not pairs:
+        return
+    max_val = max(pairs.values())
+    best_key = min(k for k, v in pairs.items() if v == max_val)
+    vocabulary.add_token(best_key)
+    print(vocabulary.encode("\n "))
+
 
 
 if __name__ == "__main__":
