9770f27cb90f071f21d2e08cc96b97dde0caa7f1
Abdelfatah  Laktaoui <alaktaou@c4r5p1.1337.ma>
2026-09-05 19:16:55 +0100

    implement tokenizer serialization

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -1,4 +1,6 @@
-from typing import Dict, Tuple, Optional
+from typing import Dict, Tuple, Optional, Any, List
+
+import json
 from collections import defaultdict
 from itertools import pairwise
 
@@ -7,26 +9,41 @@ from .vocabulary import Vocabulary
 
 
 class Tokenizer:
-    def __init__(self, vocabulary_size: int):
+    def __init__(self, vocabulary_size: int = 50000):
         self.pre_tokenize = PreTokenizer()
         self.vocab = Vocabulary()
 
-        self.vocabulary_size = vocabulary_size - 256
-        self.merges = dict()
+        self.vocabulary_size = vocabulary_size
+        self.num_merges = self.vocabulary_size - 256
+        self.metadata = {
+            "version": "0.1.0",
+            "model_type": "byte-bpe",
+            "vocab_size": self.vocabulary_size
+            }
+        self.special_tokens = {
+            "<|endoftext|>": 50256,
+            "<pad>": 50257
+            }
 
-    def step(self):
+        self.merges: Dict[Tuple[int, int], int] = dict()
+        self.chunks: Dict[Tuple[int, ...], int] = dict()
+
+    def _step(self) -> bool:
         winner = self._get_winner_pair()
-        self.merges.add(winner)
-        new_token = vocabulary.add_token(best_key)
-        slef._rewriting(new_token)
+        if winner is None:
+            return False
+        new_token = self.vocab.add_token(winner)
+        self.merges[winner] = new_token
+        self._rewriting(winner, new_token)
+        return True
 
     def _get_winner_pair(self) -> Optional[Tuple[int, int]]:
-        pairs = defaultdict(int)
+        pairs: Dict[Tuple[int, int], int] = defaultdict(int)
 
         # 1. Accumulate pair frequencies
-        for t, count in tokens.items():
-            # pairwise(t) generates consecutive (t[i], t[i+1]) in C
-            for pair in pairwise(t):
+        for c, count in self.chunks.items():
+            # pairwise(c) generates consecutive (c[i], c[i+1]) in C
+            for pair in pairwise(c):
                 pairs[pair] += count
 
         if not pairs:
@@ -34,32 +51,104 @@ class Tokenizer:
 
         # 2. Extract max value and tie-break in pure C
         max_val = max(pairs.values())
+        if max_val < 2:
+            return None
         return min(k for k, v in pairs.items() if v == max_val)
 
-    def _rewriting(self, new_token: int):
-        for token in self.tokens:
+    def _rewriting(self,
+                   best_pair: Tuple[int, int],
+                   new_token: int) -> None:
+        new_chunks = {}
+        for chunk, count in self.chunks.items():
+            new_chunk = []
             i = 0
-            token = list(token)
-            while i < len(token):
-                if (token[i], token[i+1]) == best_pair:
-                    token[i:i+1] = new_token
-                i += 1
+            while i < len(chunk):
+                if (i < len(chunk) - 1 and
+                        (chunk[i], chunk[i + 1]) == best_pair):
+                    new_chunk.append(new_token)
+                    i += 2
+                else:
+                    new_chunk.append(chunk[i])
+                    i += 1
+            new_chunks[tuple(new_chunk)] = count
+        self.chunks = new_chunks
 
+    def _generate_json_vocab(self) -> Dict[int, List[int]]:
+        return {
+            k: list(v)
+            for k, v in self.vocab.id_to_bytes.items()
+            }
 
-    def train(self, text_data, target_vocab_size):
-        self.tokens = ptoken.pre_tokenize(text)
+    def _generate_merges(self):
+        return sorted(self.merges, key=self.merges.get)
+
+    def _generate_json_tokenizer(self) -> Dict[str, Any]:
+        tokenizer_json: Dict[str, Any] = {}
+        tokenizer_json["metadata"] = self.metadata
+        tokenizer_json["vocabulary"] = self._generate_json_vocab()
+        tokenizer_json["merges"] = self._generate_merges()
+        tokenizer_json["special_tokens"] = self.special_tokens
+        return tokenizer_json
+
+    def _load_vocab_from_json_tokenizer(self, vocab: Dict[int, List[int]]):
+        self.vocab.id_to_bytes = {int(k): bytes(v) for k, v in vocab.items()}
+        self.vocab.bytes_to_id = {bytes(v): int(k) for k, v in vocab.items()}
+
+    def _load_merges(self, merges: List[List[int]]):
+        self.merges = {(m[0], m[1]): 256 + i for i, m in enumerate(merges)}
+
+    def _rewrite_slice(self, merge, raw_text: List[int]) -> List[int]:
+        new_slice = []
         i = 0
-        while i < self.vocabulary_size:
-            self.step()
+        while i < len(raw_text):
+            if (i < len(raw_text) - 1 and
+                    (raw_text[i], raw_text[i + 1]) == merge):
+                new_slice.append(self.merges[merge])
+                i += 2
+            else:
+                new_slice.append(raw_text[i])
+                i += 1
+        return new_slice
 
-    def encode(self, text):
-        pass
+    def train(self,
+              text_data: str,
+              target_vocab_size: Optional[int] = None) -> None:
+        if target_vocab_size is not None:
+            self.vocabulary_size = target_vocab_size
+            self.num_merges = self.vocabulary_size - 256
+        self.chunks = self.pre_tokenize.pre_tokenize(text_data)
+        for _ in range(self.num_merges):
+            if not self._step():
+                break
+        self.save("tokenizer.json")
 
-    def decode(self, ids):
-        pass
+    def encode(self, text: str) -> List[int]:
+        pretokenized_slices = self.pre_tokenize.split(text)
+        raw_slices = []
+        for slice_ in pretokenized_slices:
+            raw_text = self.vocab.encode(slice_)
+            raw_slices.append(raw_text)
 
-    def save(self, file_path):
-        pass
+        list_tokens = []
+        for raw_text in raw_slices:
+            for merge in self.merges:
+                raw_text = self._rewrite_slice(merge, raw_text)
+            list_tokens.append(raw_text)
 
-    def load(self, file_path):
-        pass
+        return [token for tokens in list_tokens for token in tokens]
+
+    def decode(self, ids: List[int]) -> str:
+        return self.vocab.decode(ids)
+
+    def save(self, file_path: str):
+        dict_tokenizer = self._generate_json_tokenizer()
+        with open(file_path, "w") as f:
+            json.dump(dict_tokenizer, f, indent=4)
+
+    def load(self, file_path: str):
+        with open(file_path, "r") as f:
+            tokenizer_json = json.load(f)
+        self.special_tokens = tokenizer_json["special_tokens"]
+        self.metadata = tokenizer_json["metadata"]
+        self._load_vocab_from_json_tokenizer(tokenizer_json["vocabulary"])
+        self._load_merges(tokenizer_json["merges"])
