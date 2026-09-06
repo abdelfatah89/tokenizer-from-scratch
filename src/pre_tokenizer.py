0ea16a035b66a20a9797eb7ea319bdf32df05811
Abdelfatah  Laktaoui <alaktaou@c4r5p1.1337.ma>
2026-09-06 12:42:16 +0100

    refactor: format pre-tokenizer regex pattern

--- src/pre_tokenizer.py
+++ src/pre_tokenizer.py
@@ -4,7 +4,11 @@ import regex as re  # type: ignore[import-untyped]
 
 from .vocabulary import Vocabulary
 
-pattern = r"""'s|'t|'re|'ve|'m|'ll|'d| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+pattern = (
+    r"'s|'t|'re|'ve|'m|'ll|'d|"
+    r" ?\p{L}+| ?\p{N}+|"
+    r" ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"
+)
 
 
 class PreTokenizer:
