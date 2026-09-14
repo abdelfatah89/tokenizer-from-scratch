a227b1f835a560319149e742aeb069bdc94c55a9
Abdelfatah  Laktaoui <alaktaou@c4r5p1.1337.ma>
2026-09-14 15:54:43 +0100

    Handle program crache

--- src/__main__.py
+++ src/__main__.py
@@ -2,8 +2,13 @@ from .tokenizer import Tokenizer
 
 
 if __name__ == "__main__":
-    with open("training_data/pg84.txt", "r") as f:
-        training_data = f.read()
+    try:
+        with open("training_data/pg84.txt", "r") as f:
+            training_data = f.read()
 
-    tokenizer = Tokenizer()
-    tokenizer.train(training_data, 5000)
+        tokenizer = Tokenizer()
+        tokenizer.train(training_data, 5000)
+    except KeyboardInterrupt:
+        print("Goodbey")
+    except Exception as e:
+        print(e)
